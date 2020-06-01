630dd3e8bf784729dd35777e41253198f0b83197
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-06-01 15:24:31 +0700

    deploy

---
+++ precache-manifest.48a19702e23bf5905b3d45d658dba7f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5f6dee6b0d4a388545",
+    "url": "/demo-vuejs/css/app.711ed5ed.css"
+  },
+  {
+    "revision": "f9785e810b6d8e0aff73",
+    "url": "/demo-vuejs/css/chunk-vendors.764a149a.css"
+  },
+  {
+    "revision": "721e33b36342ecd8379e44a88dd88a20",
+    "url": "/demo-vuejs/index.html"
+  },
+  {
+    "revision": "d4853635fe1187071f16",
+    "url": "/demo-vuejs/js/about.eac0682e.js"
+  },
+  {
+    "revision": "ae5f6dee6b0d4a388545",
+    "url": "/demo-vuejs/js/app.290b3de3.js"
+  },
+  {
+    "revision": "f9785e810b6d8e0aff73",
+    "url": "/demo-vuejs/js/chunk-vendors.e96671d3.js"
+  },
+  {
+    "revision": "db7a8a414a8d292161201a6b7865b53e",
+    "url": "/demo-vuejs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/demo-vuejs/robots.txt"
+  }
+]);
